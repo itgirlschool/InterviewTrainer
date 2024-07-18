0c97c5fa18ba1b934eed3674083f15aea271235f
User Nika  Azizova <nikaazizova64@gmail.com>
2024-07-18 14:37:37 +0400

    add html structure signin

--- src/Pages/Auth/SignIn/SignIn.jsx
+++ src/Pages/Auth/SignIn/SignIn.jsx
@@ -1,4 +1,31 @@
 import "./SignIn.scss";
+
+
+
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div className="container__signin">
+      <div className="cross-wrapper__signin">
+        <div className="crossbtn__signin">
+          <span className="crossbtn-x">X</span>
+        </div>
+      </div>
+      <div className="wrapper__signin">
+        <h3 className="title__signin">Регистрация</h3>
+        <form action="" className="form-signin__registration">
+          <input type="text" placeholder="Ваше имя" className="form-signin__input" />
+          <input type="text" placeholder="Ваша фамилия" className="form-signin__input" />
+          <input type="text" placeholder="Ваш Email" className="form-signin__input" />
+          <input type="text" placeholder="Ваш пароль" className="form-signin__input" />
+          <input type="text" placeholder="Повторите пароль" className="form-signin__input" />
+          <div className="save__sighin">
+            <button type="submit" className="btn__signin">Сохранить</button>
+          </div>
+          <div className="enter__signin">
+            <p className="enter__sn">Вход</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
