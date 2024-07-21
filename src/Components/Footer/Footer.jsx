b414ae980273bc9cb18a6bb38ac537ed5f108175
AlsuGai <alsu.gai05@mail.ru>
2024-07-21 23:34:21 +0300

    add footer and header

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,32 @@
 import "./Footer.scss";
+import img_google from "../../assets/images/img_chrome.png";
+import img_insta from "../../assets/images/img_insta.png";
+import img_whatsapp from "../../assets/images/img_watsapp.png";
+import img_facebook from "../../assets/images/img_facebook.png";
+import img_telegram from "../../assets/images/img telegram.png";
+
+
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer"> 
+      <button className="footer__button">
+      <a href="https://itgirlschool.com/">
+      <img className="footer__img" src={img_google} alt="Перейти на страницу ITGIRLS">
+      </img>
+      </a>
+      </button>
+      <button className="footer__button">
+      <a href="https://www.instagram.com/itgirlschool/"><img className="footer__img" src={img_insta} alt="Перейти в инстаграмм ITGIRLS"></img></a>
+      </button>
+      <button className="footer__button">
+      <a href="https://api.whatsapp.com/send/?phone=79965221233&text&type=phone_number&app_absent=0"><img className="footer__img"  src={img_whatsapp} alt="Перейти в WhatsApp"></img></a>
+      </button>
+      <button className="footer__button">
+      <a href="https://www.facebook.com/itgirlschool"><img className="footer__img"  src={img_facebook} alt="Перейти в Facebook"></img></a>
+      </button>
+      <button className="footer__button">
+      <a href="https://t.me/itgirlschool"><img className="footer__img" src={img_telegram} alt="Перейти в Telegram"></img></a></button>
+    </div>
+  )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,20 @@
 import "./Header.scss";
+import img_logo from "../../assets/images/logo_it1.png";
+import { NavLink } from "react-router-dom";
+import img_profile from "../../assets/images/img_profile_big.png"
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+        <NavLink className="header__logo" to="/home"><img src={img_logo} alt="Перейти на главную страницу"></img></NavLink>
+        <div className="header__links">
+          <NavLink className="header__link" to="">Текст</NavLink>
+          <NavLink className="header__link" to="/instructions">Инструкции</NavLink>
+          <img src={img_profile} alt="Профиль"></img>
+        </div>
+        <div className="header__burger">
+          
+        </div>
+    </div>
+  )
 }
