677a7ed4317c49be4e24b5dfba8fbec1a2a571de
User Nika  Azizova <nikaazizova64@gmail.com>
2024-07-22 12:42:34 +0400

    delete console.log

--- src/app/store/middleware/middlewareUsers.js
+++ src/app/store/middleware/middlewareUsers.js
@@ -6,9 +6,7 @@ const middlewareUsers = (database) => {
       database.ref("users").on("value", (snapshot) => {
         const data = snapshot.val();
         store.dispatch(setUsers(data));
-        console.log('1');
       });
-      console.log('2');
     }
   };
 };
