0161129f6a5e75d16056992cf2e83dd4b74e2f40
User Nika  Azizova <nikaazizova64@gmail.com>
2024-07-22 12:40:07 +0400

    deleted empty spaces

--- src/Pages/Auth/SignIn/signin.actions.js
+++ src/Pages/Auth/SignIn/signin.actions.js
@@ -2,20 +2,15 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "../../../../firebaseConfig";
 import { addUser } from "../../../Services/fbUsers";
-
-
 initializeApp(firebaseConfig);
 const auth = getAuth();
 
 export const Register = (user) => {
 
     return async () => {
-
         createUserWithEmailAndPassword(auth, user.email, user.password)
             .then(() => {
                
-              
-                
                           const infoUser= {
                             firstName: user.firstName,
                             lastName: user.lastName,
@@ -24,7 +19,6 @@ export const Register = (user) => {
                             id: user.id,
                             password: user.password,
                           }
-
                            addUser(infoUser);
                           console.log(infoUser);
 
