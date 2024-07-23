9782f82b18f4be34ade58e57eda2d3dae84b46c2
Dinovo-Ilmira <dinovo.ilmira@gmail.com>
2024-07-23 00:24:46 -0400

    Added information to the Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,40 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import "./Home.scss";
+import { app, realtimeDb } from "../../../firebaseConfig"; 
 
 export default function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const handleSelect = (path) => {
+    navigate(path);
+  };
+
+  return (
+    <div className="home">
+      <h1 className="welcome-title">Добро пожаловать, USER!</h1>
+      <p className="intro-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
+      </p>
+      <div className="grade-container">
+        <div className="grade junior">
+          <h2>Junior</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+          <button onClick={() => handleSelect('/gradingfirst')}>Выбрать</button>
+        </div>
+        <div className="grade middle">
+          <h2>Middle</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverра maecenas accumsan lacus vel facilisis.</p>
+          <button onClick={() => handleSelect('/gradingsecond')}>Выбрать</button>
+        </div>
+        <div className="grade senior">
+          <h2>Senior</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+          <button onClick={() => handleSelect('/gradingthird')}>Выбрать</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/store/index.js
+++ src/app/store/index.js
@@ -2,14 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import userAuthSlice from "./slice/UserAuthSlice";
 import usersSlice from "./slice/UsersSlice";
 import middlewareUsers from "./middleware/middlewareUsers";
+import { realtimeDb } from "../../../firebaseConfig"; 
 
-import firebase from "firebase/compat/app";
-import firebaseConfig from "../../../firebaseConfig";
-import "firebase/compat/database";
-import { initializeApp } from "firebase/app";
-
-initializeApp(firebaseConfig);
-export const database = firebase.initializeApp(firebaseConfig).database();
+export const database = realtimeDb;
 
 export const store = configureStore({
   reducer: {
