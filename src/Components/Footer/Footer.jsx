a4e9b7015617e36933db27253e5d68b665f56e72
AlsuGai <alsu.gai05@mail.ru>
2024-07-25 20:18:29 +0300

    add mobile and tablet version

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,32 +1,30 @@
 import "./Footer.scss";
-import img_google from "../../assets/images/img_chrome.png";
-import img_insta from "../../assets/images/img_insta.png";
-import img_whatsapp from "../../assets/images/img_watsapp.png";
-import img_facebook from "../../assets/images/img_facebook.png";
-import img_telegram from "../../assets/images/img telegram.png";
-
-
+import img_google from "../../assets/images/img_google.svg";
+import img_insta from "../../assets/images/img_insta.svg";
+import img_whatsapp from "../../assets/images/img_whatsapp.svg";
+import img_facebook from "../../assets/images/img_facebook.svg";
+import img_telegram from "../../assets/images/img_telegram.svg";
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer className="footer"> 
       <button className="footer__button">
-      <a href="https://itgirlschool.com/">
-      <img className="footer__img" src={img_google} alt="Перейти на страницу ITGIRLS">
-      </img>
-      </a>
+      <NavLink target="_blank" to="https://itgirlschool.com/">
+      <img className="footer__img" src={img_google} alt="Перейти на страницу ITGIRLS" />
+      </NavLink>
       </button>
       <button className="footer__button">
-      <a href="https://www.instagram.com/itgirlschool/"><img className="footer__img" src={img_insta} alt="Перейти в инстаграмм ITGIRLS"></img></a>
+      <NavLink target="_blank" to="https://www.instagram.com/itgirlschool/"><img className="footer__img" src={img_insta} alt="Перейти в инстаграмм ITGIRLS" /></NavLink>
       </button>
       <button className="footer__button">
-      <a href="https://api.whatsapp.com/send/?phone=79965221233&text&type=phone_number&app_absent=0"><img className="footer__img"  src={img_whatsapp} alt="Перейти в WhatsApp"></img></a>
+      <NavLink target="_blank" to="https://api.whatsapp.com/send/?phone=79965221233&text&type=phone_number&app_absent=0"><img className="footer__img"  src={img_whatsapp} alt="Перейти в WhatsApp" /></NavLink>
       </button>
       <button className="footer__button">
-      <a href="https://www.facebook.com/itgirlschool"><img className="footer__img"  src={img_facebook} alt="Перейти в Facebook"></img></a>
+      <NavLink target="_blank" to="https://www.facebook.com/itgirlschool" ><img className="footer__img"  src={img_facebook} alt="Перейти в Facebook" /></NavLink>
       </button>
       <button className="footer__button">
-      <a href="https://t.me/itgirlschool"><img className="footer__img" src={img_telegram} alt="Перейти в Telegram"></img></a></button>
+      <NavLink target="_blank" to="https://t.me/itgirlschool"><img className="footer__img" src={img_telegram} alt="Перейти в Telegram" /></NavLink></button>
     </footer>
   )
 }
