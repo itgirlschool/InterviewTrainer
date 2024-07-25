1f0463448c3619deb9b8d4cc45c515272cb7db9b
Dinovo-Ilmira <dinovo.ilmira@gmail.com>
2024-07-25 01:09:54 -0400

    Implemented responsive design for home page

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -2,20 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import userAuthSlice from "./slice/UserAuthSlice";
 import usersSlice from "./slice/UsersSlice";
 import middlewareUsers from "./middleware/middlewareUsers";
+import { app, realtimeDb } from "../../../firebaseConfig"; 
 
-import firebase from "firebase/compat/app";
-import firebaseConfig from "../../../firebaseConfig";
-import "firebase/compat/database";
-import { initializeApp } from "firebase/app";
-
-initializeApp(firebaseConfig);
-export const database = firebase.initializeApp(firebaseConfig).database();
+export const database = realtimeDb;
 
 export const store = configureStore({
   reducer: {
     userAuth: userAuthSlice,
     users: usersSlice,
   },
-  middleware: (getDefaultMiddleWare) =>
-    getDefaultMiddleWare().concat(middlewareUsers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middlewareUsers),
 });
