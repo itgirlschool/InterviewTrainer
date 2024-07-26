a182b9b40228748d9ba6aa8ffdebdf9598d26333
Dinovo-Ilmira <dinovo.ilmira@gmail.com>
2024-07-26 13:12:10 -0400

    Resolve merge conflicts and fix firebaseConfig import

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -2,10 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import userAuthSlice from "./slice/UserAuthSlice";
 import usersSlice from "./slice/UsersSlice";
 import middlewareUsers from "./middleware/middlewareUsers";
-<<<<<<< HEAD
-
-=======
->>>>>>> b2615a3d948aafe4cf29c4a60cba9abadb1c68a1
 import firebase from "firebase/compat/app";
 import firebaseConfig from "../../../firebaseConfig";
 import "firebase/compat/database";
@@ -15,20 +11,15 @@ initializeApp(firebaseConfig);
 export const database = firebase.initializeApp(firebaseConfig).database();
 const listenerMiddlewareUsers = middlewareUsers(database);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> b2615a3d948aafe4cf29c4a60cba9abadb1c68a1
 export const store = configureStore({
   reducer: {
     userAuth: userAuthSlice,
     users: usersSlice,
   },
-<<<<<<< HEAD
+
   middleware: (getDefaultMiddleWare) =>
     getDefaultMiddleWare().concat(listenerMiddlewareUsers),
-=======
+
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(listenerMiddlewareUsers),
->>>>>>> b2615a3d948aafe4cf29c4a60cba9abadb1c68a1
 });
