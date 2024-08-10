8cbf9c59a769b20b9e2e44e7afa76dfc6d980fd3
Daria <scheglova.daria@gmail.com>
2024-08-10 22:53:19 +0300

    add player icons && import to VideoPlayer

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,8 +1,13 @@
 import React, { useRef } from "react";
+import useVideoPlayer from "../../hooks/useVideoPlayer.js";
+import "./VideoPlayer.scss";
+import play from "../../assets/images/player_play.svg";
+import pause from "../../assets/images/player_pause.svg";
+import mute from "../../assets/images/player_mute.svg";
+import unmute from "../../assets/images/player_unmute.svg";
 
 function VideoPlayer() {
   const videoElement = useRef(null);
-
   return (
     <React.Fragment>
       <video
