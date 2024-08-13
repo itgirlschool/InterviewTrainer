1083f35a7595711cbf45851bfd5e839ccf4e6261
Daria <scheglova.daria@gmail.com>
2024-08-13 19:40:48 +0300

    del VPlayer from scratch && left video tag w/controls && add VPagination

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,44 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import VideoPlayerInner from "./VideoPlayerInner.jsx";
-// import VideoPlayerInner from "./VideoPlayerCustomize.jsx";
+import VideoPlayerPagination from "./VideoPlayerPagination.jsx";
 import "./VideoPlayer.scss";
-import prev from "../../assets/images/video_arr-prev.svg";
-import next from "../../assets/images/video_arr-next.svg";
-import check from "../../assets/images/video_checked.svg";
 
-function VideoTest() {
-  const [isChecked, setIsChecked] = useState(false);
-  const handleCheck = () => {
-    setIsChecked(true);
-  };
-
+function VideoPlayer() {
   return (
     <React.Fragment>
       <div className="container">
         <VideoPlayerInner src="https://www.dropbox.com/scl/fi/wju7gayfi0px8x1u4vcch/.mp4?rlkey=kv78rg1o21jzpueqjp5xpa3zl&st=h242md6n&dl=1" />
-        <div className="video__nav">
-          <button className="video__nav-prev">
-            <img src={prev} alt="video-prev" />
-            <p>Предыдущее видео</p>
-          </button>
-          {!isChecked ? (
-            <button
-              className="video__nav-finish"
-              onClick={handleCheck}
-            >
-              <img src={check} alt="video-checked" />
-              <p>Я посмотрела</p>
-            </button>
-          ) : (
-            <button className="video__nav-next">
-              <p>Следующее видео</p>
-              <img src={next} alt="video-next" />
-            </button>
-          )}
-        </div>
+        <VideoPlayerPagination />
       </div>
     </React.Fragment>
   );
 }
 
-export default VideoTest;
+export default VideoPlayer;
