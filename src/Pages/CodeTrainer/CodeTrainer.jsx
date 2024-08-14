23c6b4580a6d6c1b0f6e1b0ec94ac0e92708b764
Daria Kot <poturayko@gmail.com>
2024-08-14 16:48:18 +0200

    added textarea to CodeTrainer

--- src/Pages/CodeTrainer/CodeTrainer.jsx
+++ src/Pages/CodeTrainer/CodeTrainer.jsx
@@ -1,9 +1,14 @@
 import "./CodeTrainer.scss";
+import tasks from "../../data/tasks.json";
 
 export function CodeTrainer() {
 
+    const firstTask = tasks.length > 0 ? tasks[0].task : ""; 
+
     return (
+        <>
     <h1>Code Trainer</h1>
-    );
+    <textarea name="task" id="task" cols="30" rows="10" value={firstTask}></textarea>
+    </> );
 }
 
