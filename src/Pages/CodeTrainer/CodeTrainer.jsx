1ae3dd0ed881878d297d3161a7cfdc68d823472f
AlsuGai <alsu.gai05@mail.ru>
2024-08-15 17:17:30 +0300

    add codemirror library and styles

--- src/Pages/CodeTrainer/CodeTrainer.jsx
+++ src/Pages/CodeTrainer/CodeTrainer.jsx
@@ -1,19 +1,36 @@
 import "./CodeTrainer.scss";
-import tasks from "../../data/tasks.json";
+import img_lamp from "../../assets/images/img_lamp.svg";
+import Trainer from "../../Components/Trainer/Trainer";
+import { useState } from "react";
+import tasksJSON from "../../data/tasks.json";
+import { NavLink } from "react-router-dom";
 
 export function CodeTrainer() {
+  const [tasks, setTasks] = useState(tasksJSON);
+  const [solution, setSolution] = useState(false);
+  console.log(tasks);
+  //const [count, setCount] = useState(0);
 
-    const firstTask = tasks.length > 0 ? tasks[0].task : ""; 
-
-    return (
-        <>
-    <h1>Code Trainer</h1>
-    <div className="container">
-        <div className="task__container">
-            <div className="task__text">{firstTask}</div>
+  if (!tasks) {
+    return <h2>Ошибка</h2>;
+  }
+  return (
+    <div className="grading__codetasks">
+      <div className="grading__intro">
+        <NavLink to="/gradingsecond" className="graiding__name">
+          Градация 2
+        </NavLink>
+        <div className="graiding__section">&gt; CodeTrainer</div>
+      </div>
+      <div className="codetrainer">
+        <Trainer />
+        <div className="codetrainer__buttons">
+          <button className="codetrainer__solution">
+            Посмотреть решение <img src={img_lamp} alt="Посмотреть решение" />
+          </button>
+          <button className="codetrainer__check">Проверить</button>
         </div>
-        <textarea className="textarea" name="solution" id="solution"></textarea>
+      </div>
     </div>
-    </> );
+  );
 }
-
