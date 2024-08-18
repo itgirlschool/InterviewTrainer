fc4cc886974b773f34f1de202656307d2ae955bd
AlsuGai <alsu.gai05@mail.ru>
2024-08-18 23:56:28 +0300

    create buttons

--- src/Components/Trainer/Trainer.jsx
+++ src/Components/Trainer/Trainer.jsx
@@ -1,11 +1,12 @@
 import "./Trainer.scss";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef, useEffect } from "react";
 import { dracula } from "@uiw/codemirror-theme-dracula";
 
-export default function Trainer() {
-  //const { task, funcName, solution } = tasks;
+export default function Trainer( ) {
+  const [answer, setAnswer] = useState(false);
+  const refBtn = useRef();
   const [value, setValue] = useState("console.log('hello world!');");
   const onChange = useCallback((val, viewUpdate) => {
     console.log("val:", val);
@@ -17,13 +18,9 @@ export default function Trainer() {
         <div className="trainer__question">
           <div className="trainer__name">Палиндром</div>
           <div className="trainer__theme">JavaScript</div>
-          <div className="task__text">
-            Создать функцию, которая будет проверять, является ли строка
-            палиндромом (одинаково читается справа и слева) и возвращать булевое
-          </div>
-          <pre className="task__example">
-            Пример: isPalindrome(‘тест’); //false; isPalindrome(‘шалаш’); //true
-          </pre>
+          <div className="task__text">Создать функцию, которая будет проверять, является ли строка палиндромом (одинаково читается справа и слева) и возвращать булевое значение.</div>
+          <pre className="task__example">Пример: isPalindrome(‘тест’); //false; isPalindrome(‘шалаш’); //true</pre>
+          <pre className="task__answer active">function isPalindrome</pre>
         </div>
         <CodeMirror
           theme={dracula}
--- src/Pages/CodeTrainer/CodeTrainer.jsx
+++ src/Pages/CodeTrainer/CodeTrainer.jsx
@@ -1,4 +1,7 @@
 import "./CodeTrainer.scss";
+import {
+  ArrowRightOutlined
+} from "@ant-design/icons";
 import img_lamp from "../../assets/images/img_lamp.svg";
 import Trainer from "../../Components/Trainer/Trainer";
 import { useState } from "react";
@@ -7,10 +10,27 @@ import { NavLink } from "react-router-dom";
 
 export function CodeTrainer() {
   const [tasks, setTasks] = useState(tasksJSON);
-  const [solution, setSolution] = useState(false);
   console.log(tasks);
-  //const [count, setCount] = useState(0);
+  const [isClick, setIsClick] = useState(false)
+ 
+  const checkTask = (
+    <div className="codetrainer__button">
+    <button 
+          className="codetrainer__check"
+          onClick={() => setIsClick(!isClick)}>Проверить</button>
+    </div>
+  )
 
+  const viewSolution = (
+    <div className="codetrainer__buttons">
+      <button className="codetrainer__pink">
+            Посмотреть решение <img src={img_lamp} alt="Посмотреть решение" />
+          </button>
+    <button 
+          className="codetrainer__pink"
+        >Следующий <ArrowRightOutlined /></button>
+  </div>)
+  
   if (!tasks) {
     return <h2>Ошибка</h2>;
   }
@@ -24,11 +44,9 @@ export function CodeTrainer() {
       </div>
       <div className="codetrainer">
         <Trainer />
-        <div className="codetrainer__buttons">
-          <button className="codetrainer__solution">
-            Посмотреть решение <img src={img_lamp} alt="Посмотреть решение" />
-          </button>
-          <button className="codetrainer__check">Проверить</button>
+        <div className="codetaining__buttom">
+        <div></div>
+        {!isClick ? checkTask : viewSolution}
         </div>
       </div>
     </div>
