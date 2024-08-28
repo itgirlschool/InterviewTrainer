6b9449898a1704f70157388a78e7e79a289b7ad6
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-08-28 17:34:03 +0300

    feat: add functionality to prev and next buttons in video pagination

--- src/Components/VideoPlayer/VideoPlayerInner.jsx
+++ src/Components/VideoPlayer/VideoPlayerInner.jsx
@@ -1,17 +1,9 @@
-import React from "react";
 import "./VideoPlayer.scss";
 
 function VideoPlayerInner({ src, videoRef }) {
-  const handleError = () => {
-    console.error("Failed to load video:", src);
-  };
-
   return (
     <div className="video__wrapper">
-      <video
-        ref={videoRef}
-        controls /* onError={handleError}*/
-      >
+      <video ref={videoRef} controls>
         <source src={src} type="video/mp4" />
         <p>Ваш браузер не поддерживает видео</p>
       </video>
--- src/Components/VideoPlayer/VideoPlayerPagination.jsx
+++ src/Components/VideoPlayer/VideoPlayerPagination.jsx
@@ -1,19 +1,44 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./VideoPlayer.scss";
 import prev from "../../assets/images/video_arr-prev.svg";
 import next from "../../assets/images/video_arr-next.svg";
 import check from "../../assets/images/video_checked.svg";
 import catBottomPic from "../../assets/images/background_cat-video.svg";
+import { useSelector } from "react-redux";
 
 function VideoPlayerPagination({ isEnded }) {
   const [hasWatched, setHasWatched] = useState(false);
+  const data = useSelector(state => state.videos.videos);
+  const { id } = useParams();
+  const currentVideo = parseInt(id, 10);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setHasWatched(false);
+  }, [id]);
 
   const handleCheck = () => {
     if (isEnded) setHasWatched(true);
   };
 
-  const watching = (
+  const handlePrev = () => {
+    if (currentVideo > 1) {
+      navigate(
+        `/gradingfirst/videofirst/${currentVideo - 1}`,
+      );
+    }
+  };
+
+  const handleNext = () => {
+    if (currentVideo < data.length) {
+      navigate(
+        `/gradingfirst/videofirst/${currentVideo + 1}`,
+      );
+    }
+  };
+
+  const watchingBtn = (
     <button
       className={`video__button ${
         !isEnded ? "disabled" : ""
@@ -26,10 +51,12 @@ function VideoPlayerPagination({ isEnded }) {
     </button>
   );
 
-  const watched = (
+  const watchedBtn = (
     <button
-      className="video__button"
-      disabled={!isEnded}
+      className={`video__button ${
+        currentVideo === data.length ? "disabled" : ""
+      }`}
+      disabled={currentVideo === data.length}
       onClick={handleNext}
     >
       <img src={next} alt="video-next" />
@@ -37,24 +64,34 @@ function VideoPlayerPagination({ isEnded }) {
     </button>
   );
 
+  const prevBtn = (
+    <button
+      className={`video__button ${
+        currentVideo === 1 ? "disabled" : ""
+      }`}
+      onClick={handlePrev}
+      disabled={currentVideo === 1}
+    >
+      <img src={prev} alt="video-prev" />
+      <p>Предыдущее видео</p>
+    </button>
+  );
+
+  const catImg = (
+    <div className="video__cat">
+      <img
+        className="bg-image__cat"
+        src={catBottomPic}
+        alt="video_cat"
+      />
+    </div>
+  );
+
   return (
     <div className="video__nav">
-      <button
-        className="video__button"
-        onClick={handlePrev}
-      >
-        <img src={prev} alt="video-prev" />
-        <p>Предыдущее видео</p>
-      </button>
-      <div className="video__cat">
-        <img
-          className="bg-image__cat"
-          src={catBottomPic}
-          alt="video_cat"
-        />
-      </div>
-
-      {hasWatched && isEnded ? watched : watching}
+      {prevBtn}
+      {catImg}
+      {hasWatched ? watchedBtn : watchingBtn}
     </div>
   );
 }
