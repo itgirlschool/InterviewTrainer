a36f86e4a9379dce935dd721cac75412864a4d0a
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-09-03 09:25:01 +0300

    refactor: modify styles for autotests container

--- src/Pages/AutoTests/AutoTests.jsx
+++ src/Pages/AutoTests/AutoTests.jsx
@@ -5,12 +5,10 @@ import './AutoTests.scss'
 
 export default function AutoTests() {
 
-    return (
-        <div className='container-autotests'>
-           <div className='title-autotests'>
-            <NavLink to="#" >Вернуться к градации</NavLink>
-            </div>
-            <Slider />
-        </div>
-    )
+  return (
+    <div className='autotests'>
+      <NavLink className='autotests__title' to="#" >Вернуться к градации</NavLink>
+      <Slider />
+    </div>
+  )
 }
