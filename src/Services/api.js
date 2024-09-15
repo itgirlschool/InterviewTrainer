bc1be10d512707fe4c740eedcf27d9173812a6c6
fukuniji <marusina.nat@gmail.com>
2024-09-15 16:35:35 +0500

    refactor: change fetch to axios

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,24 +1,29 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "/api/v2/piston",
+});
+
 export const executeCode = async (userCode) => {
   try {
-    const response = await fetch("/api/v2/piston/execute", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        language: "javascript",
-        version: "*",
-        files: [{ name: "userCode.js", content: userCode }],
-      }),
+    const response = await API.post("/execute", {
+      language: "javascript",
+      version: "*",
+      files: [
+        {
+          name: "userCode.js",
+          content: userCode,
+        },
+      ],
     });
-
-    if (!response.ok) {
-      throw new Error(`Ошибка API: ${response.status}`);
-    }
-
-    const result = await response.json();
-    return result.run;
+    return response.data.run;
   } catch (error) {
-    throw new Error(`Ошибка выполнения кода: ${error.message}`);
+    if (error.response) {
+      console.error(`Ошибка с кодом ${error.response.status}: ${error.response.data}`);
+      throw new Error(`Ошибка выполнения кода: ${error.response.data}`);
+    } else {
+      console.error("Ошибка сети или запроса:", error.message);
+      throw new Error(`Ошибка сети или запроса: ${error.message}`);
+    }
   }
 };
