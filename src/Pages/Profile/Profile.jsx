549b2e07252f07f2461d2df6330ce2115424f61b
telinv <linatorgashova2002@mail.ru>
2024-09-27 13:24:23 +0300

    made blocks in profile to fill in

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,5 +1,14 @@
 import "./Profile.scss";
+import React from "react";
 
 export default function Profile() {
-  return <div>Profile</div>;
-}
+  return (
+  <div className="container__main">
+  <div className="welcome__block"></div>
+  <div className="container__info">
+  <div className="progress__block"></div>
+  <div className="user__info__block">
+  </div>
+  </div>
+  </div>
+)}
