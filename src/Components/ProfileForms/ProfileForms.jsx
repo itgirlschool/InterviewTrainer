0544cdedf3f100f8de390302f44388522f88e5f3
fukuniji <marusina.nat@gmail.com>
2024-10-21 16:17:43 +0500

    feat: add Tooltip and shadows to gear in ProfileForms

--- src/Components/ProfileForms/ProfileForms.jsx
+++ src/Components/ProfileForms/ProfileForms.jsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { Tooltip } from "antd";
 import ProfileForm from "./ProfileForm";
 import PasswordForm from "./PasswordForm";
 import "./ProfileForms.scss";
 
 const ProfileForms = () => {
   const [isChangingPassword, setIsChangingPassword] = useState(false);
-
+  const customTooltipColor = "#ea5d80"
   const user = useSelector((state) => state.userAuth);
 
   return (
     <div className="profile__forms__container">
       <div className="profile__forms__wrapper">
         <h2 className="profile__forms__heading">{isChangingPassword ? "Пароль" : "Профиль"}</h2>
-        <div className="settings__icon" onClick={() => setIsChangingPassword(!isChangingPassword)} title="Изменить пароль">
-          <img src="src/assets/images/settings.svg" alt="Настройки пароля" />
-        </div>
+        <Tooltip title="Изменить пароль" color={customTooltipColor}>
+          <div className="settings__icon" onClick={() => setIsChangingPassword(!isChangingPassword)}>
+            <img src="src/assets/images/settings.svg" alt="Настройки пароля" />
+          </div>
+        </Tooltip>
       </div>
       <div className="profile__forms">
         {!isChangingPassword ? (
