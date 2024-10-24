6033de0b2d3ca4a41b017dd4b7149f8404a81b77
AlsuGai <alsu.gai05@mail.ru>
2024-10-24 23:41:36 +0300

    edit home home page, login

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,9 +3,8 @@ import { getDatabase } from "firebase/database";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./Home.scss";
-import cat_grade from "../../assets/images/cat_page_grades.svg";
 import arrow_heart from "../../assets/images/arrow_heart.svg";
-import salute from "../../assets/images/lines.png";
+import cat from "../../assets/images/cat_home.png";
 import { app, realtimeDb } from "../../../firebaseConfig";
 import { useDispatch } from "react-redux";
 import { setUser } from "../../app/store/slice/UserAuthSlice";
@@ -61,172 +60,70 @@ export default function Home() {
   return (
     <div className="home">
       <div className="home__intro">
-        <div className="welcome__title">
-          <h1>Выбор градации</h1>
-          <p className="welcome__text">
-            Тренажер предлагает практические задания и вопросы, которые помогут вам
-            подготовиться к собеседованиям разного уровня — от Junior до Senior. Начните
-            подготовку к собеседованию уже сегодня и сделайте шаг к успешной карьере в
-            сфере frontend-разработки! Начни с уровня Intern.
-          </p>
+        <div>
+          <div className="welcome__title">
+            <h1>Начнем готовиться к собеседованию</h1>
+            <p className="welcome__text">
+              Тренажер предлагает практические задания и вопросы, которые помогут вам
+              подготовиться к собеседованиям разного уровня — от Junior до Senior. Начните
+              подготовку к собеседованию уже сегодня и сделайте шаг к успешной карьере в
+              сфере frontend-разработки!
+            </p>
+          </div>
+          <div className="grade intern">
+            <h2>Front-end developer</h2>
+            {progressIntern != 0 ? (
+              <div className="progressBar">
+                <div className="progressBar__title">
+                  <span>Пройдено: </span>
+                  <span className="progressBar__value">{progressIntern}</span>
+                  <span>%</span>
+                </div>
+                <div className="progressBar__outer">
+                  <div
+                    className="progressBar__inner"
+                    style={{ width: `${progressIntern}%` }}
+                  ></div>
+                </div>
+              </div>
+            ) : (
+              <div className="progressBar"></div>
+            )}
+            <p>
+              Используя наш тренажер ты сможешь закрепить знания по основным принципам
+              веб-разработки, такие как HTML, CSS и JavaScript. Ты познакомишься с
+              типичными вопросами и заданиями, которые могут быть заданы на
+              собеседованиях, для начинающих разработчиков.
+            </p>
+            {progressIntern === 0 ? (
+              <div className="block__button">
+                <button
+                  className="home__button"
+                  onClick={() => handleSelect("/gradingfirst")}
+                >
+                  Выбрать
+                </button>
+              </div>
+            ) : (
+              <div className="block__button">
+                <button
+                  className="home__button"
+                  onClick={() => handleSelect("/gradingfirst")}
+                >
+                  Продолжить
+                </button>
+                <button
+                  className="button_startOver"
+                  onClick={() => handleStartOver("gradingfirst")}
+                >
+                  Начать сначала
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-        <img className="cat__arrow" src={arrow_heart} alt="Выберите градацию" />
-        <img className="cat__withlaptop" src={cat_grade} alt="Выберите градацию" />
-      </div>
-      <div className="grade-container">
-        <div className="grade intern">
-          <h2>Intern</h2>
-          {progressIntern != 0 ? (
-            <div className="progressBar">
-              <div className="progressBar__title">
-                <span>Пройдено: </span>
-                <span className="progressBar__value">{progressIntern}</span>
-                <span>%</span>
-              </div>
-              <div className="progressBar__outer">
-                <div
-                  className="progressBar__inner"
-                  style={{ width: `${progressIntern}%` }}
-                ></div>
-              </div>
-            </div>
-          ) : (
-            <div className="progressBar"></div>
-          )}
-          <p>
-            В этой градации ты сможешь закрепить знания по основным принципам
-            веб-разработки, такие как HTML, CSS и JavaScript. Ты познакомишься с типичными
-            вопросами и заданиями, которые могут быть заданы на собеседованиях, для
-            начинающих разработчиков.
-          </p>
-          {progressIntern === 0 ? (
-            <div className="block__button">
-              <button
-                className="home__button"
-                onClick={() => handleSelect("/gradingfirst")}
-              >
-                Выбрать
-              </button>
-            </div>
-          ) : (
-            <div className="block__button">
-              <button
-                className="home__button"
-                onClick={() => handleSelect("/gradingfirst")}
-              >
-                Продолжить
-              </button>
-              <button
-                className="button_startOver"
-                onClick={() => handleStartOver("gradingfirst")}
-              >
-                Начать сначала
-              </button>
-            </div>
-          )}
-        </div>
-        <div className="grade junior">
-          <h2>Junior</h2>
-          {progressJunior != 0 ? (
-            <div className="progressBar">
-              <div className="progressBar__title">
-                <span>Пройдено: </span>
-                <span className="progressBar__value">{progressJunior}</span>
-                <span>%</span>
-              </div>
-              <div className="progressBar__outer">
-                <div
-                  className="progressBar__inner"
-                  style={{ width: `${progressJunior}%` }}
-                ></div>
-              </div>
-            </div>
-          ) : (
-            <div className="progressBar"></div>
-          )}
-          <p>
-            Тренажер включает более сложные вопросы, касающиеся разных технологий
-            (например, React, TypeScript), задачи на оптимизацию производительности и
-            работу с API. Ты сможешь изучить вопросы, касающиеся проектирования приложений
-            и выбора технологий, что важно для среднего уровня.
-          </p>
-          {progressJunior === 0 ? (
-            <div className="block__button">
-              <button
-                className="home__button"
-                onClick={() => handleSelect("/gradingsecond")}
-              >
-                Выбрать
-              </button>
-            </div>
-          ) : (
-            <div className="block__button">
-              <button
-                className="home__button"
-                onClick={() => handleSelect("/gradingsecond")}
-              >
-                Продолжить
-              </button>
-              <button
-                className="button_startOver"
-                onClick={() => handleStartOver("gradingsecond")}
-              >
-                Начать сначала
-              </button>
-            </div>
-          )}
-        </div>
-        <div className="grade middle">
-          <h2>Middle</h2>
-          {progressMiddle != 0 ? (
-            <div className="progressBar">
-              <div className="progressBar__title">
-                <span>Пройдено: </span>
-                <span className="progressBar__value">{progressMiddle}</span>
-                <span>%</span>
-              </div>
-              <div className="progressBar__outer">
-                <div
-                  className="progressBar__inner"
-                  style={{ width: `${progressMiddle}%` }}
-                ></div>
-              </div>
-            </div>
-          ) : (
-            <div className="progressBar"></div>
-          )}
-          <p>
-            Ты будешь сталкиваться с вопросами, касающ имися проектирования систем,
-            управления командами и решения сложных задач. Тренажер поможет развить навыки
-            эффективной коммуникации и лидерства, что является ключевым для
-            senior-разработчиков, отвечающих за направления своей команды.
-          </p>
-          {progressMiddle === 0 ? (
-            <div className="block__button">
-              <button
-                className="home__button"
-                onClick={() => handleSelect("/gradingthird")}
-              >
-                Выбрать
-              </button>
-            </div>
-          ) : (
-            <div className="block__button">
-              <button
-                className="home__button"
-                onClick={() => handleSelect("/gradingthird")}
-              >
-                Продолжить
-              </button>
-              <button
-                className="button_startOver"
-                onClick={() => handleStartOver("gradingthird")}
-              >
-                Начать сначала
-              </button>
-            </div>
-          )}
-        </div>
+        <img className="cat__arrow" src={arrow_heart} alt="Стрелка" />
+        <img className="cat" src={cat} alt="Кот с компьютером" />
       </div>
     </div>
   );
